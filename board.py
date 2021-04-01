7f2ecaf7701db5205c1329d85bc5e3c7e4cf93c9
Themba <thembangubeni04@gmail.com>
2021-04-01 05:59:04 +0200

    Uploaded to the last state of final submit.

--- board.py
+++ board.py
@@ -16,15 +16,31 @@ def generate(column: int, row: int, empty=False) -> list:
 
 def header(board: list) -> str:
     header_string = ""
-    for i, row in enumerate(board):
+    for i, row in enumerate(board[0]):
         header_string += f"{' ' * 5}{i}" if i == 0 else f"{' ' * 3}{i}"
     return f"{header_string}\n {'-' * (len(header_string)+1)}\n"
 
 
-def display(board: list):
+def display(board: list) -> str:
     board_string = header(board)
     for row_index, row in enumerate(board):
         for column_index, column in enumerate(row):
             board_string += f" {row_index} | {column} |" if column_index == 0 else f" {column} |"
         board_string += "\n"
     return board_string
+
+
+def row_exists(board: list, row: int) -> bool:
+    return 0 <= row < len(board)
+
+
+def column_exists(board: list, column: int) -> bool:
+    return 0 <= column < len(board[0])
+
+
+def make_move(board: list, row: int, column: int) -> int:
+    return board[row][column]
+
+
+def is_bomb(box: int) -> bool:
+    return box == 0
--- main.py
+++ main.py
@@ -1,38 +1,50 @@
+import unicode
 import board
 from game_state import GameState
 
 
-# Initialize Game State
+# Initialize GameState to key track if the game has ended.
 game_state = GameState()
 
-print("Welcome to WeThinkCode (Group 26) MineSweeper\n"
-      "Rules:\n"
-      f"Min Columns {board.MIN_NUMBER_COLUMNS}\n"
-      f"Min Rows {board.MIN_NUMBER_ROWS}\n")
+print(" Welcome to WeThinkCode (Group 26) MineSweeper.\n\n"
+      "-----------------------------------------------------------\n"
+      "| Rules:" + (" " * 50) + "|\n"
+      "-----------------------------------------------------------\n"
+      f"| * Number of rows must be greater then or equal to {board.MIN_NUMBER_COLUMNS}." + (" " * 4) + "|\n"
+      f"| * Number of columns must be greater then or equal to {board.MIN_NUMBER_ROWS}. |\n"
+      "| * Rows " + unicode.ARROW_DOWN + (" " * 48) + "|\n"
+      "| * Columns " + unicode.ARROW_RIGHT + (" " * 45) + "|\n"
+      "-----------------------------------------------------------\n")
 
-columns = 4  # int(input("Enter the number of columns: "))
-rows = 4  # int(input("Enter the number of rows: "))
-
+columns = int(input("Enter the number of columns: "))
+rows = int(input("Enter the number of rows: "))
 
 if columns < board.MIN_NUMBER_COLUMNS and rows < board.MIN_NUMBER_ROWS:
     print(f"Please re-enter the correct number of rows or columns.")
     exit()
 
 game_board = board.generate(columns, rows)
-sample_board = board.generate(columns, rows, True)
+display_board = board.generate(columns, rows, True)
+
+
+def copy_box(board: list, row: int, column: int, value: int) -> list:
+    board[row][column] = value
+    return board
 
 
 while game_state.state():
-      pass
-
-
-
-
-
-
-
-
-# print(board.display(game_board))
-# print(board.display(sample_board))
-
-
+    print(board.display(display_board))
+    selected_row = int(input("Select row: "))
+    if board.row_exists(game_board, selected_row) is False:
+        print(f"The row selected ({selected_row}) row does not exists.\n\n")
+        continue
+    selected_column = int(input("Select column: "))
+    if board.column_exists(game_board, selected_column) is False:
+        print(f"The column ({selected_column}) selected does not exists.\n\n")
+        continue
+    opened_box = board.make_move(game_board, selected_row, selected_column)
+    if board.is_bomb(opened_box):
+        print(board.display(game_board))
+        print(f"Sorry you have opened a bomb " + unicode.BOOM)
+        exit()
+    display_board = copy_box(display_board, selected_row, selected_column, opened_box)
